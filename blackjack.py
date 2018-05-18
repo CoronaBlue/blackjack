ae8f05361ffdbf066e7d0bb9b3d276086f2c396d
Adam Huffman <coronablue3@gmail.com>
2018-05-18 17:55:18 -0600

    Created blackjack.py
    Created the main .py file for the game. I've included a bit of pseudo-code to help me organize my thoughts.

---
+++ blackjack.py
@@ -0,0 +1,59 @@
+'''
+Name: Blackjack
+Author: Adam Huffman
+Date: 5/18/17
+Description: Text based Blackjack game, where one player faces off against an AI-controlled dealer.
+'''
+
+# Player class
+# Needs a hand
+# Needs a bankroll
+# Needs to be able to bet
+# Needs to be able to draw
+# Needs to be able to stay
+
+# Dealer class
+# Needs a hand
+# Needs to be able to draw
+
+# Card class
+# Needs a suit
+# Needs a value
+# Needs to be able to flip
+
+# Deck Class
+# Needs 52 Cards
+# Needs to be able to shuffle
+# Needs to be able to be drawn from
+
+################## Get Objects Ready #######################
+# Player
+# Dealer
+# Deck
+
+################# Game Starts Here ###########################
+
+# WHILE TRUE
+  
+  # Shuffle the deck.
+  
+  # Player places a bet.
+  
+  # Player gets one Card from the Deck, face up.
+  # Dealer gets one Card from the Deck, face up.
+  # Player gets one Card from the Deck, face up.
+  # Dealer gets one Card from the Deck, face down
+  
+  # Display cards, and calculate totals.
+  
+  # Is there a BUST, or a NATURAL?
+  
+  # While the player has not STAYED.
+    
+    # Player chooses to hit, or stay.
+    
+    # If the player hits...
+      
+      # The player draws a Card.
+      
+      # Did they bust, or reach 21?
